5c1b1dfde36a73b489705690718033ac81982cb6
Alejandro <goofyahead@gmail.com>
2014-02-22 17:58:38 +0100

    track data

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var bleno = require('bleno');
+var BlenoPrimaryService = bleno.PrimaryService;
+var BlenoCharacteristic = bleno.Characteristic;
+var BlenoDescriptor = bleno.Descriptor;
 
 console.log('bleno - iBeacon');
 
@@ -6,7 +9,8 @@ bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertisingIBeacon('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
+    // bleno.startAdvertisingIBeacon('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
+    bleno.startAdvertising('mglData', [fffffffffffffffffffffffffffffff0]);
   } else {
     bleno.stopAdvertising();
   }
@@ -19,3 +23,33 @@ bleno.on('advertisingStart', function() {
 bleno.on('advertisingStop', function() {
   console.log('on -> advertisingStop');
 });
+
+function SampleService() {
+  SampleService.super_.call(this, {
+    uuid: 'fffffffffffffffffffffffffffffff0',
+    characteristics: [ 
+      new DynamicReadOnlyCharacteristic()
+    ]
+  });
+}
+
+var DynamicReadOnlyCharacteristic = function() {
+  DynamicReadOnlyCharacteristic.super_.call(this, {
+    uuid: 'fffffffffffffffffffffffffffffff2',
+    properties: ['read']
+  });
+};
+
+util.inherits(DynamicReadOnlyCharacteristic, BlenoCharacteristic);
+
+DynamicReadOnlyCharacteristic.prototype.onReadRequest = function(offset, callback) {
+  var result = this.RESULT_SUCCESS;
+  var data = new Buffer('dynamic value');
+
+  if (offset > data.length) {
+    result = this.RESULT_INVALID_OFFSET;
+    data = null;
+  }
+
+  callback(result, data);
+};
